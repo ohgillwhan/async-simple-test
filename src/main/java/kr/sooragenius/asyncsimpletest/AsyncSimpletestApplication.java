c6b823e487c3f51b93cc3dd8c5d4de1842a3f2da
ohgillwhan <soora_genius@naver.com>
2020-10-05 22:54:43 +0900

    Schedule 적용 spring.task.scheduling.pool.size 적용

--- src/main/java/kr/sooragenius/asyncsimpletest/AsyncSimpletestApplication.java
+++ src/main/java/kr/sooragenius/asyncsimpletest/AsyncSimpletestApplication.java
@@ -6,14 +6,16 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 //@EnableAsync
+@EnableScheduling
 public class AsyncSimpletestApplication implements ApplicationRunner {
-    @Autowired
-    private ASync1 aSync1;
-    @Autowired
-    private ASync2 aSync2;
+//    @Autowired
+//    private ASync1 aSync1;
+//    @Autowired
+//    private ASync2 aSync2;
 
     public static void main(String[] args) {
         SpringApplication.run(AsyncSimpletestApplication.class, args);
@@ -21,9 +23,10 @@ public class AsyncSimpletestApplication implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        aSync1.test();
-        aSync2.test();
-        for(int i = 0; i<1000000; i++)
-        System.out.println("End");
+        Thread.sleep(10000);
+//        aSync1.test();
+//        aSync2.test();
+//        for(int i = 0; i<1000000; i++)
+//        System.out.println("End");
     }
 }
---
+++ src/main/java/kr/sooragenius/asyncsimpletest/Schedule2.java
@@ -0,0 +1,18 @@
+package kr.sooragenius.asyncsimpletest;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Schedule2 {
+    @Scheduled(fixedDelay = 100L)
+    public void hello() {
+        System.out.println(Thread.currentThread().getName());
+        System.out.println("Schedule2");
+        try {
+            Thread.sleep(100);
+        }catch(Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
